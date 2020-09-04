0bf68290341aada3bbe1d9e9a43d0a13933cd13d
Saxavlax001 <saxavlax001@gmail.com>
2020-09-04 14:19:20 +0300

    pmmp's scoreboard api part 1

--- src/pocketmine/network/protocol/types/ScoreboardIdentityPacketEntry.php
+++ src/pocketmine/network/protocol/types/ScoreboardIdentityPacketEntry.php
@@ -23,23 +23,10 @@ declare(strict_types=1);
 
 namespace pocketmine\network\protocol\types;
 
-class ScorePacketEntry{
-	const TYPE_PLAYER = 1;
-	const TYPE_ENTITY = 2;
-	const TYPE_FAKE_PLAYER = 3;
-
+class ScoreboardIdentityPacketEntry{
 	/** @var int */
 	public $scoreboardId;
-	/** @var string */
-	public $objectiveName;
-	/** @var int */
-	public $score;
+	/** @var int|null */
+	public $entityUniqueId = null;
 
-	/** @var int */
-	public $type;
-
-	/** @var int|null (if type entity or player) */
-	public $entityUniqueId;
-	/** @var string|null (if type fake player) */
-	public $customName;
 }
